ca9fd78c87acebc6c73c2f4b713f4876ff486c3e
xdedss <liuzhuoran_2000@163.com>
2021-07-07 08:48:49 +0800

    add glm

--- pointcloud.h
+++ pointcloud.h
@@ -9,9 +9,9 @@
 #include <qopenglbuffer.h>
 
 #include <vector>
-#include <glm.hpp>
-#include <gtc/matrix_transform.hpp>
-#include <gtc/type_ptr.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "vertex.h"
 
--- widget.h
+++ widget.h
@@ -12,9 +12,9 @@
 #include <qopenglvertexarrayobject.h>
 #include <qopenglbuffer.h>
 // GLM
-#include <glm.hpp>
-#include <gtc/matrix_transform.hpp>
-#include <gtc/type_ptr.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include <QDebug>
 #include <QKeyEvent>
