90366d81a784552f945b5e5eff69cc5c5cc8c8b6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-07-20 07:10:10 +0000

    Deploy xdedss/gldemo to github.com/xdedss/gldemo.git:gh-pages

--- search/all_b.js
+++ search/all_b.js
@@ -2,8 +2,12 @@ var searchData=
 [
   ['obj2index',['obj2index',['../class_hierarchy_model.html#a3d82905f2cca890de32e00305207823c',1,'HierarchyModel']]],
   ['objectcount',['objectCount',['../class_hierarchy_model.html#aea68e82fd678cc829ed60c7f98fc1746',1,'HierarchyModel']]],
+  ['offrecordvideo2recordwindow',['offRecordVideo2recordWindow',['../class_main_window.html#a0057cda47372be62b6a050c06a4ca8a8',1,'MainWindow']]],
+  ['offsavevideo2recordwindow',['offSaveVideo2recordWindow',['../class_main_window.html#a20b044ffe67541e6c7a3b96ae9a82999',1,'MainWindow']]],
   ['onpropertychange',['onPropertyChange',['../class_component.html#a35debebcd31a06c01a8cffd398750608',1,'Component::onPropertyChange()'],['../class_line_renderer.html#aa63c3368f1299a89381d36e90597af42',1,'LineRenderer::onPropertyChange()']]],
+  ['onrecordvideo2widget',['onRecordVideo2Widget',['../class_main_window.html#a441b9d3e8ac1dffa88b2bc3ba7a4ea48',1,'MainWindow']]],
   ['onrender',['onRender',['../class_line_renderer.html#ac1086db7ffa80ceda6bc6cfaf3e104dc',1,'LineRenderer::onRender()'],['../class_point_cloud_renderer.html#a2e785d573849a727a30f536a420052c5',1,'PointCloudRenderer::onRender()'],['../class_renderer.html#a42088d22a1d0e4463cb7f4541cbf187f',1,'Renderer::onRender()'],['../class_skybox_renderer.html#ab5d9e816496a6d88038aedc09389c03b',1,'SkyboxRenderer::onRender()']]],
+  ['onsavevideo2widget',['onSaveVideo2Widget',['../class_main_window.html#a10ace3477b859a250f2db041eb64b7af',1,'MainWindow']]],
   ['onselection',['onSelection',['../class_widget.html#a760a209487d5c5b0ff08d38304279f4b',1,'Widget']]],
   ['ontransformedited',['onTransformEdited',['../class_widget.html#a20e05cae601a3e5ec101d9268985c1e5',1,'Widget']]],
   ['onupdate',['onUpdate',['../class_component.html#a1c028f8e10929d098b9275f6f4dd40c3',1,'Component::onUpdate()'],['../class_renderer.html#ac3f10732d5c9350623dcac4d039e07ee',1,'Renderer::onUpdate()'],['../class_trail.html#aaee6b727969ee5b265dd812dc93b843a',1,'Trail::onUpdate()']]]
