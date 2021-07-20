56c3bb9a06fca504ab0aaecaa11977541379c20f
xdedss <liuzhuoran_2000@163.com>
2021-07-20 16:03:09 +0800

    main page

--- main.cpp
+++ main.cpp
@@ -3,12 +3,6 @@
 
 #include <QApplication>
 
-/** \mainpage 基于OpenGL和Qt的3D点云渲染 - documentation
- *
- *  See:
- *   - <a href="modules.html" >C++ API organized by modules</a>
- *   - <a href="https://github.com/jlblancoc/nanoflann" >Online README</a>
- */
 
 // 程序入口  
 int main(int argc, char *argv[])
